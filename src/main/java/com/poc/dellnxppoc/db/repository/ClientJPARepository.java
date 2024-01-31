ac2a1d6ea71f0b46374b9ce2ae5a0eae8518c476
Nikhil Naik <nikhil.naik@afourtech.com>
2024-01-31 10:45:05 +0530

    Correcting package and file naming

--- src/main/java/com/poc/dellnxppoc/db/repository/ClientJPARepository.java
+++ src/main/java/com/poc/dellnxppoc/db/repository/ClientJPARepository.java
@@ -3,9 +3,9 @@ package com.poc.dellnxppoc.db.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.poc.dellnxppoc.db.dto.ClientJpaDTO;
+import com.poc.dellnxppoc.db.model.ClientJpaEntity;
 
 @Repository
-public interface ClientJPARepository extends JpaRepository<ClientJpaDTO, String> {
+public interface ClientJPARepository extends JpaRepository<ClientJpaEntity, String> {
 
 }
